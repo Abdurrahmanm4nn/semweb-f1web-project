721f76cee9bd013a8499d31ae966cb0e5e99c78e
Rahman <abdurrahman1270@gmail.com>
2022-05-29 17:27:50 +0700

    Can Show data in browser page

--- routes/data.js
+++ routes/data.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var d3 = require('d3-sparql');
 var { SparqlEndpointFetcher } = require('fetch-sparql-endpoint');
 var { SparqlJsonParser } = require('sparqljson-parse');
 const myFetcher = new SparqlEndpointFetcher;
 const myParser = new SparqlJsonParser;
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-    let dataFetch = await myFetcher.fetchBindings('http://localhost:3030/test_dataset/sparql', 'PREFIX d: <http://aghniyaabdurrahman.com/data#> PREFIX dsc: <http://aghniyaabdurrahman.com/description#>   SELECT ?friend ?name ?birthPlace ?contact ?email  WHERE {     ?x  dsc:friend  ?friend;         dsc:name  ?name;         dsc:birthPlace  ?birthPlace;         dsc:contact  ?contact;         dsc:email  ?email.     }');
-    dataFetch.on('data', (bindings) => {
-        let result = myParser.parseJsonBindings(bindings);
-        return res.json(result);
-    });
+router.get('/', async function(req, res, next) {   
+    let query = `
+    PREFIX d: <http://semanticf1web.com/data#>
+    PREFIX dsc: <http://semanticf1web.com/description#>
+
+    SELECT ?team ?name ?country ?birthdate ?age ?instagram
+    WHERE {
+        ?x  dsc:team  ?team;
+            dsc:name  ?name;
+            dsc:country ?country;
+            dsc:birthdate  ?birthdate;
+            dsc:age  ?age;
+            dsc:instagram  ?instagram.
+    }`;
+    let sparqlEndpoint = 'http://localhost:3030/f1driverdata/sparql';
+
+    let result = await d3.sparql(sparqlEndpoint, query);
+    let data = JSON.stringify(result);
     
-    //res.send(dataFetch.data);
+    return res.status(200).render('test', {drivers: result});
 });
-  
-  module.exports = router;
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var d3 = require('d3-sparql');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  res.render('index', { title: 'Express'});
+router.get('/', async function(req, res, next) {   
+  let query = `
+  PREFIX d: <http://semanticf1web.com/data#>
+  PREFIX dsc: <http://semanticf1web.com/description#>
+
+  SELECT ?team ?name ?country ?birthdate ?age ?instagram
+  WHERE {
+      ?x  dsc:team  ?team;
+          dsc:name  ?name;
+          dsc:country ?country;
+          dsc:birthdate  ?birthdate;
+          dsc:age  ?age;
+          dsc:instagram  ?instagram.
+  }`;
+  let sparqlEndpoint = 'http://localhost:3030/f1driverdata/sparql';
+
+  let result = await d3.sparql(sparqlEndpoint, query);
+  let data = JSON.stringify(result);
+  
+  return res.status(200).render('test', {drivers: result});
 });
+
 
 module.exports = router;
